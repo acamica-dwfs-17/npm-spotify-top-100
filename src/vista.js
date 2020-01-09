7447482f57c669b5537ad4b41d4b4ffc44d56af2
Adrian Tucci <adriantucci@MacBook-Pro-de-Adrian.local>
2020-01-09 19:45:38 -0300

    mensajito BOKEEEEEE

--- src/vista.js
+++ src/vista.js
@@ -1,7 +1,5 @@
 import Spotify from 'spotify-web-api-js';
 
-import bootstrap from 'bootstrap';
-import $ from 'jquery';
 import qs from 'query-string';
 import './styles/main.scss';
 
@@ -19,7 +17,7 @@ const links = [
 ];
 
 const client_id = 'e0d81b145f7844b8b4fc10e579daf34a';
-const client_secret = '';
+const client_secret = 'e57b801ce6ed45e885d697e4d4bd4cac';
 
 async function main() {
   NavBar('titulo', links);
@@ -27,15 +25,17 @@ async function main() {
   let params = qs.parse(window.location.search);
   let hasCode = !!params.code;
   if (hasCode) {
+
       let s = new Spotify()
       let response = await fetch('https://accounts.spotify.com/api/token', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
         },
-        body:`grant_type=authorization_code&code=${params.code}&redirect_uri=${encodeURIComponent(window.location.origin)}&client_id=${client_id}&client_secret=${client_secret}`
+        body:`grant_type=authorization_code&code=${params.code}&redirect_uri=${encodeURIComponent(window.location.origin)}/&client_id=${client_id}&client_secret=${client_secret}`
     }).then(x=>x.json())
-    console.log(response);
+    
+    
     
   } else {
     const authBase = 'https://accounts.spotify.com/authorize?';
